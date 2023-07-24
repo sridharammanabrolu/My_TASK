0eb69c61d5fa1f053a60176a28a60b59232218b1
ammanabrolusridhar <sridhar@bassure.com>
2023-07-24 11:41:13 +0530

    changes

---
+++ ippopay/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Ippopay_Password from './Ippopay_Password';
+import Ippopay_Task2 from './Ippopay_Task2';
+
+function App() {
+  return (
+    <div className="App">
+      <Ippopay_Password />
+      {/* <Ippopay_Task2 /> */}
+    </div>
+  );
+}
+
+export default App;
